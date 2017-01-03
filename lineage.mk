bc92593c0fc33850d7d734c2c837a77505157ccf
David Hays <dhays90@gmail.com>
2017-01-02 19:39:48 -0600

    himaul: rebrand to LineageOS
    Change-Id: I42ed2dfaffbbc34a8373f59812bf42a8b06e996a

--- lineage.mk
+++ lineage.mk
@@ -1,9 +1,9 @@
 $(call inherit-product, device/htc/himaul/full_himaul.mk)
 
-# Inherit some common CM stuff.
+# Inherit some common Lineage stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
-PRODUCT_NAME := cm_himaul
+PRODUCT_NAME := lineage_himaul
 BOARD_VENDOR := htc
 TARGET_VENDOR := htc
 PRODUCT_DEVICE := himaul
