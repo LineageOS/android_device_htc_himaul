38efd778a91db57ff74dd49aaac5c85b9c550988
razorloves <razorloves@gmail.com>
2018-04-02 18:03:32 -0500

    himaul: set build fingerprint instead of override
    Change-Id: Iee35bc558d44baf45d0c735ac44e64a1060f4f03

--- lineage.mk
+++ lineage.mk
@@ -11,5 +11,6 @@ PRODUCT_DEVICE := himaul
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_DEVICE="htc_himaulatt" \
     PRODUCT_NAME="himaulatt_na_gen_unlock" \
-    BUILD_FINGERPRINT="htc/himaulatt_na_gen_unlock/htc_himaulatt:7.0/NRD90M/870623.7:user/release-keys" \
     PRIVATE_BUILD_DESC="4.27.502.7 CL870623 release-keys"
+
+BUILD_FINGERPRINT := "htc/himaulatt_na_gen_unlock/htc_himaulatt:7.0/NRD90M/870623.7:user/release-keys"
